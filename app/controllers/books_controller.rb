fc10356671a4e335941372b45fe66680f4eea232
kn1025yu <sn22kzh@gamil.com>
2021-10-22 13:24:01 +0000

    [add]indexの編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,26 @@
 class BooksController < ApplicationController
+  def index
+    @book = Book.all
+    book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
+  def show
+  end
+
+  def destory
+  end
+
+  private
+
+  def post_image_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
