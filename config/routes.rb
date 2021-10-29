da43592bc43cdab87c6bed8743c78dde18fc2de2
kn1025yu <sn22kzh@gamil.com>
2021-10-29 08:21:14 +0000

    [fix]ユーザ編集画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions', registrations: "users/registrations",
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'about' => 'homes#about'
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
   post '/books/new', to: 'books#create', as: :create
 end
