dbc51e18ae51b742192dccb815b1a862c00fc954
kn1025yu <sn22kzh@gamil.com>
2021-11-06 07:09:34 +0000

    [fix]課題3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,7 +27,6 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
     if @book.user == current_user
       render :edit
     else
@@ -36,7 +35,6 @@ class BooksController < ApplicationController
   end
 
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
     redirect_to book_path(@book), notice:'successfully'
     else
@@ -47,7 +45,6 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
@@ -55,6 +52,10 @@ class BooksController < ApplicationController
 private
   def book_params
    params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
   end
 
 end
